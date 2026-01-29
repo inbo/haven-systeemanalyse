5ba471c21af688eec66f060b05222a0c396d70d7
GEUNENS <oberon.geunens@inbo.be>
2026-01-29 18:11:37 +0100

    Add new lib_utility and use check_lintr()

--- source/libraries/Load_packages.R
+++ source/libraries/Load_packages.R
@@ -1,24 +1,23 @@
-###Load/install all packages-------------------------------------------------------------------------------------------
-#Add new packages here if necessary. The code will check and download new ones and load everything.
-# Install the packages 
-allPackages <- c(
-  
+###Load/install all packages -------------------
+# Add new packages here if necessary.
+# The code will download new ones and load everything.
+all_packages <- c(
   # General
   "DBI", "conflicted", "units", "checklist",
   # INBO-packages
-  "watina", "inbodb", "inbolims", "INBOtheme", 
+  "watina", "inbodb", "inbolims", "INBOtheme",
   # Data wrangling
   "tidyverse", "glue", "lubridate", "santoku",
   # Spatial data
-  "sf", "terra", 
+  "sf", "terra",
   # Visualisation
-  "ggforce", "patchwork", "tidyterra", "ggspatial", "ggnewscale", "ggsflabel", "maptiles", "scales",
+  "ggforce", "patchwork", "tidyterra", "ggspatial", "ggnewscale",
+  "ggsflabel", "maptiles", "scales",
   # external
   "writexl", "readxl", "officer", "flextable")
 
-
-for (package in allPackages){
-  if (!require(package, character.only = TRUE)){
+for (package in all_packages) {
+  if (!require(package, character.only = TRUE)) {
     install.packages(package, dependencies = TRUE)
   }
   library(package, character.only = TRUE)
@@ -32,31 +31,6 @@ conflicts_prefer(glue::trim)
 conflicts_prefer(ggplot2::geom_sf_label)
 conflicts_prefer(readxl::read_xlsx)
 
-RoundAny = function(x, accuracy, f = round){
-  # Rond een getal af op een in te geven cijfer.
-  # x is het getal, accuracy de cijfer waarnaar het dient af te ronden
-  # f de formule voor ceiling (dichtbijzijnde naar boven afgerond) of floor (dichtsbijzijnde naar onder afgerond).
-  
-  f(x/accuracy) * accuracy
-}
-
-NullVariable <- function(variable) {
-  # Make NA variable NULL
-  if (is.na(variable)) {
-    variable <- NULL
-  }
-  
-  return(variable)
-}
-
-CreatePath <- function(path, subfolder) {
-  
-  new_path <- str_c(path, subfolder, sep = "/")
-  if (!file.exists(new_path)) {
-    dir.create(new_path)
-  }
-  return(new_path)
-}
 check_spelling()
 check_filename()
 check_lintr()
---
+++ source/libraries/lib_utility.R
@@ -0,0 +1,26 @@
+round_any <- function(x, accuracy, f = round) {
+  # Oude naamgeving:  RoundAny
+  # Rond een getal af op een in te geven cijfer.
+  # x is het getal, accuracy de cijfer waarnaar het dient af te ronden
+  # f de formule voor ceiling (dichtbijzijnde naar boven afgerond) of
+  # floor (dichtsbijzijnde naar onder afgerond).
+  return(f(x / accuracy) * accuracy)
+}
+
+null_variable <- function(variable) {
+  # Old function name = "NullVariable"
+  # Make NA variable NULL
+  if (is.na(variable)) {
+    variable <- NULL
+  }
+  return(variable)
+}
+
+create_path <- function(path, subfolder) {
+  # old name = CreatePath
+  new_path <- str_c(path, subfolder, sep = "/")
+  if (!file.exists(new_path)) {
+    dir.create(new_path)
+  }
+  return(new_path)
+}
